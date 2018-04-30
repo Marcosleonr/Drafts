908392278498d7bcfc588f63d3a7736fa0c82002
Marcos León Reyes <37048529+mleonr@users.noreply.github.com>
2018-04-30 12:10:20 -0500

    Add files via upload

---
+++ Delete_a_Node.cpp
@@ -0,0 +1,34 @@
+Node* Delete(Node* head, int position){
+	
+	int i = 0, n = 0, cont = 0;
+	Node *ans, *aux1, *previous;
+	
+	if(head->next==NULL){
+		ans = NULL;
+	}else if(position==0){
+		ans = head->next;
+	}else{
+		
+		for(i=0;i<position+1;i++){
+			if(i=0){
+				aux1 = head;
+				previous = head;
+				
+			}else{
+				aux1 = aux1->next;
+				if(n==position){
+					if(aux1->next==NULL){
+						previous = NULL;
+					}else{
+						previous->next = aux1->next;
+					}
+				}
+				if(i==position-1){
+					previous = aux1;
+				}
+			}
+			
+		}	
+	}
+	return ans;
+}
